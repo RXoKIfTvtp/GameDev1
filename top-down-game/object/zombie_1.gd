76049721ee9fbac7540466675b82473e36f3d3fb
RXoKIfTvtp <112825461+RXoKIfTvtp@users.noreply.github.com>
2025-02-23 16:37:49 -0800

    Added sprites and zombies that chase the player
    The zombies chase the player to the player's last seen location. If the line of sight is broken the zombies stop chasing the player. The line of sight is a simple ray trace and does not yet implement a field of view.

---
+++ top-down-game/object/zombie_1.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+const SPEED = 200;
+
+var _target = null;
+
+func player_position(pos: Vector2) -> void:
+	# var direction = self.position.direction_to(_target).normalized();
+	var space_state = self.get_world_2d().direct_space_state;
+	var query = PhysicsRayQueryParameters2D.create(self.position, pos);
+	query.hit_from_inside = false;
+	query.collide_with_bodies = true;
+	query.collide_with_areas = true;
+	query.exclude = [ self ];
+	var result = space_state.intersect_ray(query);
+	if result && result.collider.name.to_lower() == "Player".to_lower():
+		_target = pos;
+	pass
+
+func _ready() -> void:
+	self.add_to_group("enemy");
+
+func _process(_delta: float) -> void:
+	if (_target != null):
+		if self.position.distance_to(_target) < 8:
+			_target = null;
+		else:
+			self.look_at(_target);
+		pass
+	pass
+
+func _physics_process(delta: float) -> void:
+	if (_target != null):
+		# self.position += self.position.direction_to(_target).normalized() * delta * SPEED;
+		self.velocity = self.position.direction_to(_target).normalized() * delta * SPEED * 60;
+	else:
+		self.velocity = Vector2(0, 0)
+		pass
+	move_and_slide()
