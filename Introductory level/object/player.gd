d0b14a292fb4770d6e4aa7293ce679151caf8768
Tashc110 <ctgamerpro10@gmail.com>
2025-03-11 16:32:25 -0700

    Introductory Level , changes to the physics layers
    Physics layers updated 
    Tile map being used v3

---
+++ Introductory level/object/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+func _process(_delta: float) -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide();
+	pass
+
+# Currently not used for anything
+func _on_entered(area: Area2D) -> void:
+	print("Ouch: " + str(area));
+	pass
