557ba20236dfe2bb6c1562a8717a310418b61550
RXoKIfTvtp <112825461+RXoKIfTvtp@users.noreply.github.com>
2025-03-23 13:27:44 -0700

    Updated README.md with Eric's notes

--- top-down-game/object/interactable.gd
+++ top-down-game/object/interactable.gd
@@ -1,13 +1,11 @@
 class_name Interactable extends Area2D
 
-
-enum Interaction_Types{
+enum Interaction_Types {
 	PICKUP_ITEM,
 	PICKUP_WEAPON,
 	OPEN_UNLOCKED_DOOR,
 	OPEN_LOCKED_DOOR
 }
-
 
 @export var interact_label := "none";
 @export var interact_type : Interaction_Types;
