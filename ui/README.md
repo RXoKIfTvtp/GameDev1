36f15b298761387f1a6645de8f558655e1a44eca
RXoKIfTvtp <112825461+RXoKIfTvtp@users.noreply.github.com>
2025-04-02 18:22:58 -0700

    Delete ui directory
    Accidentally added this to wrong directory.

--- ui/README.md
+++
@@ -1,5 +0,0 @@
-
-# UI
-
-This folder will contain user interface scenes and scripts, including menus and huds.
-
--- ui/settings_menu.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_back_button_pressed() -> void:
-	SceneLoader.switch_scene("res://ui/main_menu.tscn");
